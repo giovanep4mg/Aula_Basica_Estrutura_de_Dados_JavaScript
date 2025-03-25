f11ff7c938bc15b92e67c1180cc411f5f0716223
giovanep4mg <giovaneteodoro79@gmail.com>
2025-03-25 07:21:24 -0300

    pagina lista de compras finalizada

--- Mini_Projetos/Projetos_Arrays/Lista_de_Compras/javascript.js
+++ Mini_Projetos/Projetos_Arrays/Lista_de_Compras/javascript.js
@@ -33,12 +33,26 @@ function adicionarProduto() {
         // Pega o que foi escrito pelo usuário, e coloca dentro do elemento "li" => <li> ***  </li>
         li.textContent = produto;
 
+        // criando o botão remover
         // Cria elemento "button" , salva na var "botaoRemover" => <button></button>
         let botaoRemover = document.createElement('button');
         // coloca esse desenho, no botao que foi criado => <button> ❌ </button>
         botaoRemover.textContent = '❌';
-        // Adiciona uma pequena margin entre o texto e o desenho
-        botaoRemover.style.marginLeft = '15px';
+
+        // Adicionando a classe ao botão
+        botaoRemover.classList.add('botao-remover');
+
+        /**
+         * // Adicionando estilos ao botão
+        botaoRemover.style.marginLeft = '65%';
+        botaoRemover.style.width = '10%'; // Aumenta a largura do botão
+        botaoRemover.style.fontSize = '5vw'; // Aumenta o tamanho do ícone
+        botaoRemover.style.cursor = 'pointer';
+        botaoRemover.style.border = 'none'; // Remove a borda para um visual mais limpo
+        botaoRemover.style.background = 'transparent'; // Fundo transparente
+        botaoRemover.style.color = 'red'; // Deixa o ícone vermelho para destacar
+         */
+         
 
         // Adiciona uma função, dentro do botão remover, que recebe dois parâmetros "produto" e "li"
         // =>> <button onclick="removerProduto(produto, li)"> ❌ </button>
@@ -48,30 +62,36 @@ function adicionarProduto() {
             removerProduto(produto, li);
         };
 
-        //
+        // adiciona o botão dentro do elemento "li"
         li.appendChild(botaoRemover);
 
+        // Adiciona o "li" na página html, no elemento "ul"
         document.getElementById("listaProdutos").appendChild(li);
 
 
     } else {
-
+        // Exibe essa mensagem 
         alert(`Erro ao adicionar produto!Digite um nome válido! `);
     }
     
 }
 
+// Função para remover um item "produto" e o elemento  do html "tag li"
 function removerProduto(produto, elemento) {
     
-    //
+    // Verifica em que posição está o item e salva na var "index", caso nao encontre retorna "-1"
     let index = lista.indexOf(produto);
 
+    // Se não for "-1" , vai ...
     if (index !== -1) {
-        //
+        
+        // remove 1 item da lista na posição index.
         lista.splice(index, 1);
     }
-    console.log("Lista após exclussão: ", lista);
-    //
+    // Exibe essa mensagem no console
+    console.log("Lista após exclusão: ", lista);
+
+    // Remove o elemento "li" do html, onde está o item "produto"
     elemento.remove() //
 }
 
